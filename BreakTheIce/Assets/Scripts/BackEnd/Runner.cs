6682fc8b31a915f92e0332dfbf03fc5e49803efd
Tim Reinagel <tmr2_handbell@yahoo.com>
2018-05-07 22:27:57 -0700

    Runners can have money, health, and brain damage.

---
+++ BreakTheIce/Assets/Scripts/BackEnd/Runner.cs
@@ -0,0 +1,15 @@
+﻿public class Runner
+{
+    public int Money;
+    public int MaxHP;
+    public int BrainDamage;
+    public int Hp;
+    
+    public Runner(int hp, int money = 0)
+    {
+        MaxHP = hp;
+        BrainDamage = 0;
+        Hp = hp;
+        Money = money;
+    }
+}
