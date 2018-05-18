4f82c33de3fcebd7362ab6218e488ae2ce7fb012
Noah Reinagel <noah.r9@gmail.com>
2018-05-18 15:29:30 -0700

    now shows particle lines between connected nodes in server map scene

---
+++ BreakTheIce/Assets/Scripts/ParticleLineGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ParticleLineGenerator : MonoBehaviour
+    {
+        public float ParticleDistance = 1f;
+        public Vector3 RotationOffset = Vector3.zero;
+
+        public ParticleSystem System;
+        public ParticleSystemRenderer Renderer;
+        public Vector3 Source;
+        public Vector3 Dest;
+
+        private void Start ()
+        {
+            GenerateLine();
+        }
+
+        private void OnEnable()
+        {
+            GenerateLine();
+        }
+
+        private void GenerateLine()
+        {
+            var totalDistance = Vector3.Distance(Source, Dest);
+            var particleCount = (int)Mathf.Floor(totalDistance / ParticleDistance);
+            var particles = Enumerable.Range(0, particleCount).Select(x => GenerateParticle(totalDistance, x)).ToArray();
+            System.SetParticles(particles, particleCount);
+            MagicThatMakesItWork(particleCount);
+        }
+
+        private ParticleSystem.Particle GenerateParticle(float totalDistance, int index)
+        {
+            return new ParticleSystem.Particle
+            {
+                position = Vector3.MoveTowards(Source, Dest, (totalDistance % ParticleDistance) / 2 + index * ParticleDistance),
+                startSize = System.main.startSize.constant,
+                startColor = Color.white,
+                rotation3D = Quaternion.LookRotation(Source - Dest).eulerAngles + RotationOffset
+            };
+        }
+
+        private void MagicThatMakesItWork(int particleCount)
+        {
+            var customData = new List<Vector4>();
+            System.GetCustomParticleData(customData, ParticleSystemCustomData.Custom1);
+            for (int i = 0; i < particleCount; i++)
+                customData[i] = this.gameObject.transform.up;
+            System.SetCustomParticleData(customData, ParticleSystemCustomData.Custom1);
+        }
+    }
+}
--- BreakTheIce/Assets/Scripts/ServerMapSpawner.cs
+++ BreakTheIce/Assets/Scripts/ServerMapSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Assets.Scripts.BackEnd;
 using Assets.Scripts.FrontEnd;
 using UnityEngine;
@@ -7,26 +8,60 @@ namespace Assets.Scripts
 {
     public class ServerMapSpawner : MonoBehaviour
     {
-        public GameObject Barrier;
-        public GameObject Sentry;
-        public GameObject CodeGate;
-        public GameObject Boss;
-        public GameObject Checkpoint;
-        public GameObject Mystery;
+        public NodeComponent Barrier;
+        public NodeComponent Sentry;
+        public NodeComponent CodeGate;
+        public NodeComponent Boss;
+        public NodeComponent Checkpoint;
+        public ParticleLineGenerator ServerLine;
 
         public void Start()
         {
-            var nodeMap = new Dictionary<NodeType, GameObject>
-            {
-                { NodeType.Boss, Boss },
-                { NodeType.Barrier, Barrier },
-                { NodeType.CodeGate, CodeGate },
-                { NodeType.Sentry, Sentry },
-                { NodeType.Checkpoint, Checkpoint }
-            };
+            var nodeMap = CreateNodeTypeMap();
+            var nodes = SpawnNodeGameObjects(nodeMap);
+            SpawnLinesBetweenNodes(nodes);
+        }
 
-            new ServerMapLayoutCalculation(GameData.Map).Get()
-                .ForEach(x => Instantiate(nodeMap[x.Value.Type], x.Position, Quaternion.identity, transform));
+        private Dictionary<NodeType, NodeComponent> CreateNodeTypeMap()
+        {
+            return new Dictionary<NodeType, NodeComponent>
+            {
+                {NodeType.Boss, Boss},
+                {NodeType.Barrier, Barrier},
+                {NodeType.CodeGate, CodeGate},
+                {NodeType.Sentry, Sentry},
+                {NodeType.Checkpoint, Checkpoint}
+            };
+        }
+
+        private List<NodeComponent> SpawnNodeGameObjects(Dictionary<NodeType, NodeComponent> nodeMap)
+        {
+            return new ServerMapLayoutCalculation(GameData.Map).Get().Select(x =>
+                {
+                    var component = Instantiate(nodeMap[x.Value.Type], x.Position, Quaternion.identity, transform);
+                    component.Node = x.Value;
+                    return component;
+                }).ToList();
+        }
+
+        private void SpawnLinesBetweenNodes(List<NodeComponent> nodes)
+        {
+            var nodeLinesCreated = new List<List<Node>>();
+
+            nodes.ForEach(component => component.Node.Connections
+                .Where(connectedNode => !nodeLinesCreated
+                    .Any(connectedLine => connectedLine.Contains(connectedNode)
+                                       && connectedLine.Contains(component.Node)))
+                .ForEach(connectedNode => SpawnLine(nodes, nodeLinesCreated, connectedNode, component)));
+        }
+
+        private void SpawnLine(List<NodeComponent> nodes, List<List<Node>> nodeLinesCreated, Node connectedNode, NodeComponent component)
+        {
+            nodeLinesCreated.Add(new List<Node> { connectedNode, component.Node });
+            var line = Instantiate(ServerLine, transform);
+            line.Source = component.transform.position;
+            line.Dest = nodes.First(x => x.Node == connectedNode).transform.position;
+            line.gameObject.SetActive(true);
         }
     }
 }
