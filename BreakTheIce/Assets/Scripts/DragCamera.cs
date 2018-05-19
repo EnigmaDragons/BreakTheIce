f812f661f29324490c19bc90daddfde5af3aec4c
noah reinagel <noah.r9@gmail.com>
2018-05-18 23:04:36 -0700

    Add Dragging to the camera on the server map

---
+++ BreakTheIce/Assets/Scripts/DragCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class DragCamera : MonoBehaviour
+    {
+        public float DragStrength = 0.05f;
+
+        private Vector3 _positionOnScreen;
+
+	    private void Update ()
+	    {
+	        if (Input.GetMouseButtonDown(0))
+	            _positionOnScreen = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y);
+            else if (Input.GetMouseButton(0))
+	        {
+	            transform.position += (_positionOnScreen - new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y)) * DragStrength;
+	            _positionOnScreen = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y);
+	        }
+        }
+    }
+}
